7c37db38f08aa02f4211b0d05b1ef422d29ee876
Temidayomichael <dayoo.oladele@gmail.com>
2021-05-16 10:08:14 +0100

    app setup

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6e5f47cfb813b327009ceaf097117df8'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b57aa29c-e011-431c-ad43-64fe2b8153f8'
+};
