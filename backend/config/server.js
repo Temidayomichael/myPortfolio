aa4d80888d96d69d15e6e4902860a6bfad784436
Temidayomichael <dayoo.oladele@gmail.com>
2022-04-05 21:36:49 +0100

    update

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '6e5f47cfb813b327009ceaf097117df8'),
-    },
-  },
-});
